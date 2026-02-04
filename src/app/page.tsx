c6a5888a6e3cdffab94058fd480776a3adf11232
yamamoto-kai <haishanben8@gmail.com>
2026-02-04 21:03:27 +0900

    ポモドーロタイマーの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,76 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+
+type Mode = "focus" | "break";
+type Status = "idle" | "running" | "paused";
 
 export default function Home() {
+  const [count, setCount] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(0.2 * 60);
+  const [mode, setMode] = useState<Mode>("focus");
+  const [status, setStatus] = useState<Status>("idle");
+
+  const getTimeLeft = () => { 
+    const time = limit - count;
+
+    if (time <= 0) {
+      setCount(0);
+      setLimit(5 * 60);
+      setMode("break");
+      setStatus("idle");
+    };
+
+    const minute = Math.floor(time / 60);
+    const second = time % 60;
+
+    return `${minute.toString().padStart(2,"0")}:${second.toString().padStart(2,"0")}`;
+  }
+    
+  const timeLeft: string = getTimeLeft();
+
+  const handleReset = () => {
+    setCount(0);
+    setLimit(25 * 60);
+    setStatus("idle");
+  };
+
+  const handleStartTimer = () => setStatus("running");
+
+  const handleToggleTimer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setStatus(e.currentTarget.value as Status);
+  };
+  
+  useEffect(() => {
+    if (status !== "running") return;
+
+    const timerId = setInterval(() => setCount(prev => prev + 1), 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [status]);
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      <h1>{timeLeft}</h1>
+      <h2>
+        {
+          mode === "focus" ?
+            "作業中"
+            :
+            "休憩中"
+        }
+      </h2>
+      {
+        status === "idle" ?
+          <button onClick={handleStartTimer}>開始</button>
+          :
+          <>
+            <button onClick={handleToggleTimer} value={status === "running" ? "paused" : "running"}>{status === "running" ? "一時停止" : "再生"}</button>
+            <button onClick={handleReset}>リセット</button>
+          </>
+      }
+    </>
   );
 }
